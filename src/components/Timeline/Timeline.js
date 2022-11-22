977858ee3d3cffb8666365abf630468c8e1d9d9b
takumi <takura010719@gmail.com>
2022-11-22 09:38:44 +0900

    ✨ Post.jsコンポーネントにPropsを付与

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -10,12 +10,19 @@ function Timeline() {
       <div className="timeline__header">
         <h2>ホーム</h2>
       </div>
-      
+
       {/* TweetBox */}
       <TweetBox />
 
       {/* Post */}
-      <Post />
+      <Post
+        displayName="ぼく"
+        username="me"
+        verified={true}
+        text="Reactなう。"
+        avatar=""
+        image="https://source.unsplash.com/random"
+      />
     </div>
   );
 }
